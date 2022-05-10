0ae05e3b69607635f2333764e6e7395c9e3b2374
mu-se373-190706011 <101144051+mu-se373-190706011@users.noreply.github.com>
2022-05-10 23:47:53 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mobileprogramming/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Projem',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: homeScreen(),
+    );
+  }
+}
